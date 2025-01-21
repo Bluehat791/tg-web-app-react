4b9f183c3921e40ff79ce5319be91aee2fef93a0
Anatoliy Shchepetkov <tolick.shepetkov61@gmail.com>
2025-01-21 14:18:24 +0300

    New Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-const Button = (props) => {
-    return (
-        <button {...props} className={'button ' + props.className}/>
-    );
-};
+function Button({ type, title, disable, onClick }) {
+  return (
+    <button
+      className={`btn ${
+        (type === "add" && "add") ||
+        (type === "remove" && "remove") ||
+        (type === "checkout" && "checkout")
+      }`}
+      disabled={disable}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
+}
 
 export default Button;
